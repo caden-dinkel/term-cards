5c36d0ebda536a4fd8351b3e6218498b74fd4f42
Caden Dinkel <git@cdink.dev>
2026-09-16 18:19:23 -0500

    Adding basic cards abstraction

--- cards/src/lib.rs
+++ cards/src/lib.rs
@@ -2,6 +2,34 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub enum Rank {
+    Ace,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+}
+
+pub enum Suit {
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+pub struct Card {
+    suit: Suit,
+    rank: Rank,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
